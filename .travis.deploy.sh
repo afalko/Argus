e75915a0cf3905d7c6021c8b9d1fafe221512926
afalko <afalko@users.noreply.github.com>
2017-08-28 15:31:37 -0700

    Oh oh

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 cp .travis.settings.xml $HOME/.m2/settings.xml
+# Contrib here
+
 # Builds top level pom w/ zip
 # TODO: Figure out if ${PROJECT_VERSION} should be manually incremented in the POM or handled some other way
 mvn --batch-mode --non-recursive -DskipTests -DskipDockerBuild -U -Prelease -Dproject.version=${PROJECT_VERSION} clean install
 mvn --batch-mode -DskipDockerBuild -DskipTests -Dproject.version=${PROJECT_VERSION} -Prelease clean deploy
+# And here security fix from public
